0787996ae8c16b20a2fd0d9fdd344fa82d79b7df
MircoPaul <mirco.paul.1997@live.com>
2019-02-16 02:01:28 +0100

    PyCharm Debugging

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,21 +1,25 @@
 ##Multilayer-Perceptron implementation
 import numpy as np
+from numpy.core.multiarray import ndarray
+
 
 class NeuralNetwork:
+
+    totalNodes = ...  # type: ndarray
 
     def __init__(self, layers):
         self.layers = layers    ##Array storing number of i-th layer at i-th index
         self.totalNodes = np.sum(self.layers)
 
-        self.values = np.ndarray(shape=(self.totalNodes))
-        self.biases = np.ndarray(shape=(self.totalNodes))
+        self.values = np.ndarray(shape=self.totalNodes)
+        self.biases = np.ndarray(shape=self.totalNodes)
         self.weights = np.ndarray(shape=(self.totalNodes, self.totalNodes))
 
     ##Sets weights to random values and biases to 0.0
     def initialize(self):
-        for i in np.arange(self.totalNodes):
+        for i in np.arange(int(self.totalNodes)):
             self.biases[i] = 0.0
-            for j in np.arange(self.totalNodes):
+            for j in np.arange(int(self.totalNodes)):
                 self.weights[i][j] = np.random.randn(self.layers[i]) * np.sqrt(1 / self.layers[i])
 
     ##Activates the network with the current input values using activation function tanh(x)
@@ -23,10 +27,10 @@ class NeuralNetwork:
         i = self.layers[0]
         for l in np.arange(1, self.layers.size):
             for n in np.arange(self.layers[l]):
-               self. values[i] = 0.0
+                self. values[i] = 0.0
                 j = i - self.layers[l-1]
                 for p in np.arange(self.layers[l-1]):
                     self.values[i] += self.values[j] * self.weights[j, i]
                 self.values[i] += self.biases[i]
-               self.values[i] = np.tanh(self.values[i])
-                i += 1
+            self.values[i] = np.tanh(self.values[i])
+            i += 1
